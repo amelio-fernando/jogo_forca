c01acd6050098f67cc372d0f9bf7a5923a4049cd
Fernando Amelio <fernando.amelio@gmail.com>
2022-06-16 18:40:39 -0300

    primeira versao, esqueleto do jogo

--- jogo_forca.c
+++ jogo_forca.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+void abertura() {
+	printf("*************************\n");
+	printf("====> JOGO DA FORCA <====\n");
+	printf("*************************\n\n");
+}
 
 int main(){
 
-    char palavraSecreta[40];
+    int teste = 1, jogada = 0;
+    char palavraSecreta[40], chutes[26], letra;
+    sprintf(palavraSecreta, "PARALELEPIPEDO");
+
+    abertura();
+
+    do{
+       for(int i = 0; i < strlen(palavraSecreta); i++){
+
+        int acertou = 0;
+
+        if(chutes[jogada] == palavraSecreta[i]){
+            printf("%c ", palavraSecreta[i]);
+        } else {
+            printf("_ ");
+        }
 
 
+       }
+        printf("\n");
+        printf("\n");
+        printf("Digite uma letra: ");
+        scanf(" %c", &letra);
 
 
-
-
+    }while(teste = 1);
 
 return 0;
 }
