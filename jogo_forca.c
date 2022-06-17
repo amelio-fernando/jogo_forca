8a7794f8a65b51931c9da74dc9c9ccaad4a4834e
Fernando Amelio <fernando.amelio@gmail.com>
2022-06-16 22:16:06 -0300

    Primeira versão funcional

--- jogo_forca.c
+++ jogo_forca.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-void abertura() {
-	printf("*************************\n");
-	printf("====> JOGO DA FORCA <====\n");
-	printf("*************************\n\n");
+void abertura(){
+    printf("*************************\n");
+    printf("====> JOGO DA FORCA <====\n");
+    printf("*************************\n\n");
 }
 
 int main(){
 
-    int teste = 1, jogada = 0;
-    char palavraSecreta[40], chutes[26], letra;
+    int teste = 1;
+    int jogada = 0;
+    char palavraSecreta[30];
+    char chutes[26];
+    char letra;
+
     sprintf(palavraSecreta, "PARALELEPIPEDO");
 
-    abertura();
-
     do{
-       for(int i = 0; i < strlen(palavraSecreta); i++){
 
-        int acertou = 0;
+        abertura();
 
-        if(chutes[jogada] == palavraSecreta[i]){
-            printf("%c ", palavraSecreta[i]);
-        } else {
-            printf("_ ");
+        for(int i = 0; i < strlen(palavraSecreta); i++){
+
+            int achou = 0;
+
+            for(int j = 0; j < jogada; j++){
+                if(chutes[j] == palavraSecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+            if(achou){
+                printf("%c ", palavraSecreta[i]);
+            } else {
+                printf("_ ");
+            }
         }
 
 
-       }
-        printf("\n");
-        printf("\n");
-        printf("Digite uma letra: ");
-        scanf(" %c", &letra);
+            printf("\n");
+            printf("\n");
+            printf("Digite uma letra: ");
+            scanf(" %c", &letra);
+            chutes[jogada] = letra;
+            jogada++;
+            system("cls");
 
-
-    }while(teste = 1);
+ }while(teste == 1);
 
 return 0;
 }
