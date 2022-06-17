8091648ed74ee00c7c80147d4cffc21445d856b4
Fernando Amelio <fernando.amelio@gmail.com>
2022-06-17 14:28:27 -0300

    Primeiro prototipo, removido erros e acertos

--- jogo_forca.c
+++ jogo_forca.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include "forca-biblioteca.h"
 
+int jogada = 0;
+char palavraSecreta[30];
+char chutes[26];
+int acertou = 0;
+int erros = 0;
+
 void abertura(){
 
     printf("*************************\n");
@@ -16,72 +22,77 @@ void letras_chutadas(char *chutes, int *jogada){
 
     printf("Letras chutadas: ");
 
-    for(int i = 0; i < *jogada; i++){
+    for(i = 0; i < *jogada; i++){
 
-    printf(" %c ", chutes[i]);
+        printf(" %c ", chutes[i]);
 
+    }
+    printf("\n");
+    printf("\n");
+}
+
+void achou_letra (int *jogada, char *chutes, char *palavraSecreta){
+
+    for(int i = 0; i < strlen(palavraSecreta); i++){
+        int achou = 0;
+
+        for(int j = 0; j < *jogada; j++){
+
+            if(chutes[j] == palavraSecreta[i]){
+
+                achou = 1;
+                break;
+            }
+        }
+        if(achou){
+
+            printf("%c ", palavraSecreta[i]);
+        } else {
+            printf("_ ");
+          }
     }
 
     printf("\n");
+    printf("TESTE VARIAVEL ACERTOU: = %d", acertou);
     printf("\n");
+    printf("TESTE VARIAVEL ERROU: = %d", erros);
+    printf("\n");
+}
+
+void chute (char *chutes, int *jogadas){
+
+    char letra;
+
+    printf("Digite uma letra: ");
+
+    scanf(" %c", &letra);
+
+    chutes[jogada] = letra;
+
+    jogada++;
+
+    system("cls"); //esta função nao é portavel, apenas windows
+
 }
 
 int main(){
 
     int teste = 1;
-    int jogada = 0;
-    char palavraSecreta[30];
-    char chutes[26];
-    int acertos = 0;
-    int erros = 0;
 
-    sprintf(palavraSecreta, "PARALELEPIPEDO");
+    sprintf(palavraSecreta, "INCONSTITUCIONALISSIMAMENTE");
 
-    do{
+    do {
 
         abertura();
 
-        for(int i = 0; i < strlen(palavraSecreta); i++){
+        achou_letra(&jogada, &chutes, &palavraSecreta);
 
-            int achou = 0;
+        letras_chutadas(&chutes, &jogada);
 
-            for(int j = 0; j < jogada; j++){
-                if(chutes[j] == palavraSecreta[i]){
-                    achou = 1;
-                    break;
-                }
-            }
-            if(achou){
-                printf("%c ", palavraSecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-
-    letras_chutadas(&chutes, &jogada);
-
-            char letra;
-            printf("Digite uma letra: ");
-            scanf(" %c", &letra);
-            chutes[jogada] = letra;
-            jogada++;
-            system("cls"); //esta função nao é portavel, apenas windows
-
-            for(int i = 0; i < strlen(palavraSecreta); i++){
-
-                for(int j = 0; j < jogada; j++){
-                    if(chutes[j] == palavraSecreta[i]){
-                    acertos++;
-                    break;
-                    }
+        chute (&chutes, &jogada);
 
 
+    } while(teste == 1);
 
-            printf("TESTE ACERTOS = %d", acertos);
-            printf("TESTE ERROS = %d", erros);
-
-
- }while(teste == 1);
-
-return 0;
+    return 0;
 }
